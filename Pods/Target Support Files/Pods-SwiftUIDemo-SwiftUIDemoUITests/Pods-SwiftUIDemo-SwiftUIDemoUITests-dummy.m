4f286f276b1e2dc3556a27e1951e47f22cdc1c14
DK <dekai.zhang@kunlun-inc.com>
2025-01-16 16:52:01 +0800

    remove  pod

--- Pods/Target Support Files/Pods-SwiftUIDemo-SwiftUIDemoUITests/Pods-SwiftUIDemo-SwiftUIDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftUIDemo_SwiftUIDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftUIDemo_SwiftUIDemoUITests
-@end
--- Pods/Target Support Files/Pods-SwiftUIDemoTests/Pods-SwiftUIDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftUIDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftUIDemoTests
-@end
