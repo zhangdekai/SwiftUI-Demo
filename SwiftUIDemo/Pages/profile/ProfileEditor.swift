8506cb9410b4d63e92730d0f5ace40114bfd01b0
DK <dekai.zhang@kunlun-inc.com>
2025-02-07 18:06:14 +0800

    Update ReadMe; Add Profile show and Edit; add new file for @Binding

---
+++ SwiftUIDemo/Pages/profile/ProfileEditor.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditor.swift
+//  SwiftUIDemo
+//
+//  Created by zhang dekai on 2025/2/7.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditor()
+}
---
+++ SwiftUIDemo/Pages/profile/ProfileHost.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileHost.swift
+//  SwiftUIDemo
+//
+//  Created by zhang dekai on 2025/2/7.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
